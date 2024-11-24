b724242b223ff19745e70d90e533d48fc99932b8
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2024-11-24 18:43:38 +0100

    test

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = 'Mezdoud Ala Eddine';
+    ?>
+    <!-- we use the dot for concatinationg -->
+    <h1>hello my name  <?php echo "goes by" . " " . $name ?></h1>
 
-echo 'hello world';
+
+</body>
+</html>
