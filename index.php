0afbc3cb0af19eccd97b103c3e3aa82ca035e293
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2024-11-24 18:59:33 +0100

    associative arrays

--- index.php
+++ index.php
@@ -12,6 +12,48 @@
     <!-- we use the dot for concatinationg -->
     <h1>hello my name  <?php echo "goes by" . " " . $name ?></h1>
 
+    <?php 
+    
+    $books = [
+        ["title" => "I missed a prayer",
+         "genre" => "religion",
+         "price" => "12$",
+         "pages" => 445
+    ],
+        ["title" => "antikhristos",
+         "genre" => "world",
+         "price" => "11$",
+         "pages" => 362
+],
+        ["title" => "the subtle art of not giving a fuck",
+         "genre" => "selfe improvement",
+         "price" => "13$",
+         "pages" => 260
+        ]
+
+    ];
+    ?>
+
+    <table border=1>
+        <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Price</th>
+            <th>Pages</th>
+        </tr>
+        <?php 
+        foreach($books as $book){
+            echo "<tr>";
+                echo "<td>" . "$book[title]" . "</td>";
+                echo "<td>" . "$book[genre]" . "</td>";
+                echo "<td>" . "$book[price]" . "</td>";
+                echo "<td>" . "$book[pages]" . "</td>";
+            echo "</tr>";
+        }
+
+        ?>
+
+    </table>
 
 </body>
 </html>
