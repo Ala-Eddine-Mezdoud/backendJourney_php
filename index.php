7d0974b83f2d2dc1f9abc884619038a494d7f9f4
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2024-11-25 09:57:53 +0100

    views added

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        $name = 'Mezdoud Ala Eddine';
-    ?>
-    <!-- we use the dot for concatinationg -->
-    <h1>hello my name  <?php echo "goes by" . " " . $name ?></h1>
+$name = 'Mezdoud Ala Eddine';
 
-    <?php 
-    
-    $books = [
+
+$books = [
         ["title" => "I missed a prayer",
          "genre" => "religion",
          "price" => "12$",
@@ -32,39 +19,6 @@
         ]
 
     ];
-    ?>
 
-    <table border=1>
-        <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Price</th>
-            <th>Pages</th>
-        </tr>
-        <?php foreach($books as $book){ ?>
-            <tr>
-                <td> <?= $book['title'] ?>   </td>
-                <td> <?= $book['genre'] ?>   </td>
-                <td> <?= $book['price'] ?>   </td>
-                <td> <?= $book['pages'] ?>   </td>
-            </tr>
-       <?php }?>
 
-    </table>
-
-    <br>
-    <br>
-
-    <?php 
-    $myarray = [1,2,3,4,5,6,7,8,9];
-    function test($value){
-        return $value > 3;
-    }
-
-    $filteredArray = array_filter($myarray, 'test');
-    print_r($filteredArray);
-    
-    ?>
-
-</body>
-</html>
+require("views/index.view.php");
